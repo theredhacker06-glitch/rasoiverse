623938cf212e35067b0ad673e32c6bf79a4e698e
theredhacker06-glitch <theredhacker06@gmail.com>
2026-06-17 17:27:16 +0530

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+};
+
+module.exports = nextConfig;
